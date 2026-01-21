3b78cf55da672cd68167493c8b50fcc8a03d41f9
rutujaa121 <rutujaaugale@gmail.com>
2026-01-20 19:25:52 -0800

    set cookie

---
+++ Assignment-1/set_cookie.php
@@ -0,0 +1,17 @@
+<?php
+    setcookie("username", "xyz", time()+60*60);
+?>
+<html>
+    <body>
+        <?php
+            if(isset($_COOKIE["username"]))
+                {
+                    echo "Cookie is set with value: ".$_COOKIE["username"];
+                }
+            else
+                {
+                    echo "Cookie is not set";
+                }
+        ?>
+    </body>
+</html>
