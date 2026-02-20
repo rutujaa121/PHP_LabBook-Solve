c00d344d58bf2a1ec464767d57e188eef8a622cb
rutujaa121 <rutujaaugale@gmail.com>
2026-02-20 10:11:12 -0800

    added styles page first

---
+++ Assignment-1/styles.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="s2.php" method="post">
+        Enter font style <input type="text" name="f1"><br>
+        Enter font size <input type="text" name="f2"><br>
+        Enter font color <input type="text" name="f3"><br>
+        Enter Bg color <input type="text" name="f4"><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
