9547784c0154bfdde8a2e8a1d6f01870d791c7c8
rutujaa121 <rutujaaugale@gmail.com>
2026-02-22 22:32:27 -0800

    add styles page two

--- Assignment-1/s2.php
+++ Assignment-1/s2.php
@@ -1,3 +1,8 @@
 <?php
-    
+    $style = $_POST['f1'];
+    $size = $_POST['f2'];
+    $color = $_POST['f3'];
+    $bgcolor = $_POST['f4'];
+
+    echo "<p style='font-family:$style; font-size:$size; color:$color; background-color:$bgcolor;'>This is a sample text with the specified styles.</p>";
 ?>
