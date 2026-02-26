ebe44e44717c30244b64e5034975d51f0c48bc38
rutujaa121 <rutujaaugale@gmail.com>
2026-02-25 20:23:03 -0800

    update code

--- Assignment-1/s3.php
+++ Assignment-1/s3.php
@@ -3,10 +3,12 @@
     $size = $_COOKIE['f2'];
     $color = $_COOKIE['f3'];
     $bgcolor = $_COOKIE['f4'];
-
+    
+    echo "<h1> Styles </h1>";
     echo "<body bgcolor=$bgcolor>";
     echo "<font style = $style> Hello World!";
     echo "<font size=$size> Hello World!";
     echo "<font color=$color> Hello World!";
     echo "</font></body>";
+
 ?>
