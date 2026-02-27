1963b8a2811164c872aa1c664c0e25717afa4159
rutujaa121 <rutujaaugale@gmail.com>
2026-02-26 16:22:31 -0800

    added customer page 1

---
+++ customer/c1.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="c2.php" method="POST">
+        Enter Name <input type="text" name="name"><br><br>
+        Enter Address <input type="text" name="address"><br><br>
+        Enter Number <input type="text" name="phone"><br><br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
