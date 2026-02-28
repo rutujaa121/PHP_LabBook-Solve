fccf29ae0efff04f6d373a37b4eba5b261600ef6
rutujaa121 <rutujaaugale@gmail.com>
2026-02-28 09:13:13 -0800

    add c2 page

--- customer/c2.php
+++ customer/c2.php
@@ -1,4 +1,27 @@
 <?php
     session_start();
-    
+    $cname = $_POST['name'];
+    $caddr = $_POST['address'];
+    $cmob = $_POST['phone'];
+
+    $_SESSION['cname']=$came;
+    $_SESSION['address'];
+    $_SESSION['phone'];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="c3.php" method="POST">
+        <input type="text" placeholder="Enter Product Name" name="p1">
+        <input type="text" placeholder="Enter Quantity" name="p2">
+        <input type="text" placeholder="Enter Rate" name="p3">
+        <input type="submit" value="Submit">
+    </form>
+</body>
+</html>
